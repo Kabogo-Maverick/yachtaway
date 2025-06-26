e98cf0a8bdc32ddbfb3289adf7c56a7b67ef48c3
mkay <mkay@gmail.com>
2025-06-26 14:30:07 +0300

    Initial commit: project scaffold and User model migration

---
+++ server/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from server.models import User 
+import os
+from server.config import Config
+from server.models.db import db, migrate 
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    CORS(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    @app.route('/')
+    def home():
+        return {'message': 'Welcome to YachtAway API 🌊'}
+
+    return app
